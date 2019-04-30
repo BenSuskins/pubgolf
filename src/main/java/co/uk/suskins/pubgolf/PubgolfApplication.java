4b314dceb81b7b04fb56b888fbaf1c8fe0773f9d
bs16746 <B3nster9109>
2019-04-30 11:42:51 +0100

    Initial Commit from Spring Initalizr

---
+++ src/main/java/co/uk/suskins/pubgolf/PubgolfApplication.java
@@ -0,0 +1,13 @@
+package co.uk.suskins.pubgolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubgolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubgolfApplication.class, args);
+	}
+
+}
