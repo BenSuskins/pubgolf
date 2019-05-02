d0f123f96717ff700af5f8347ecb32abf8c793bd
bs16746 <B3nster9109>
2019-05-02 10:54:57 +0100

    work

--- src/main/java/co/uk/suskins/pubgolf/PubgolfApplication.java
+++ src/main/java/co/uk/suskins/pubgolf/PubgolfApplication.java
@@ -3,8 +3,8 @@ package co.uk.suskins.pubgolf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +14,16 @@ import java.security.Principal;
 @EnableOAuth2Sso
 @RestController
 public class PubgolfApplication {
-    @RequestMapping("/user")
+    @GetMapping("/user")
     public Principal user(Principal principal) {
         return principal;
     }
 
     @PostMapping("/submitscore")
-    public void submit(@RequestParam("name") String name, @RequestParam("hole") String hole, @RequestParam("par") int par) {
-        System.out.println(name);
+    public void submit(@RequestParam("name") String name,
+                       @RequestParam("hole") String hole,
+                       @RequestParam("par") String par) {
+        System.out.println(name + hole + par);
     }
 
     public static void main(String[] args) {
