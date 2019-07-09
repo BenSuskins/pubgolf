fcd4c4c7718e4fb7ae4f4b4af27e41beb3211028
Ben Suskins <suskinsdevelopment@gmail.com>
2019-07-09 23:28:05 +0100

    Cleaned up class

--- src/main/java/co/uk/suskins/pubgolf/repository/PubgolfRepository.java
+++ src/main/java/co/uk/suskins/pubgolf/repository/PubgolfRepository.java
@@ -3,6 +3,7 @@ package co.uk.suskins.pubgolf.repository;
 import co.uk.suskins.pubgolf.models.Pubgolf;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 /**
  * Interface for accessing the H2 Database.
  */
