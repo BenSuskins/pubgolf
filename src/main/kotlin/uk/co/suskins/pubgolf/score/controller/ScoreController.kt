82eb30101a452cb1cbfcff9816a4ee4f80a7bee6
Ben Suskins <suskinsdevelopment@gmail.com>
2023-07-02 22:23:23 +0100

    - Refactor

--- src/main/kotlin/uk/co/suskins/pubgolf/score/controller/ScoreController.kt
+++ src/main/kotlin/uk/co/suskins/pubgolf/score/controller/ScoreController.kt
@@ -6,12 +6,11 @@ import uk.co.suskins.pubgolf.score.models.api.ScoreDto
 import uk.co.suskins.pubgolf.score.models.mapper.ScoreMapper
 import uk.co.suskins.pubgolf.score.service.ScoreService
 
-@RestController("/api/v1/scores")
+@RestController(value = "/api/v1/scores")
 class ScoreController(val scoreService: ScoreService, val scoreMapper: ScoreMapper) {
 
     @GetMapping
-    fun getAll(): List<ScoreDto> {
-        return scoreMapper.toDtos(scoreService.getAll())
-    }
+    fun getAll(): List<ScoreDto> = scoreMapper.toDtos(scoreService.getAll())
+
 
 }
