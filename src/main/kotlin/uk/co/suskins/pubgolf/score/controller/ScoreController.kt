cdac2fb2199b210d7715d02abce88eaa096aba0a
Ben Suskins <suskinsdevelopment@gmail.com>
2023-07-02 22:25:04 +0100

    - Request Mapping for Base Path

--- src/main/kotlin/uk/co/suskins/pubgolf/score/controller/ScoreController.kt
+++ src/main/kotlin/uk/co/suskins/pubgolf/score/controller/ScoreController.kt
@@ -1,12 +1,14 @@
 package uk.co.suskins.pubgolf.score.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.co.suskins.pubgolf.score.models.api.ScoreDto
 import uk.co.suskins.pubgolf.score.models.mapper.ScoreMapper
 import uk.co.suskins.pubgolf.score.service.ScoreService
 
-@RestController(value = "/api/v1/scores")
+@RestController
+@RequestMapping("/api/v1/scores")
 class ScoreController(val scoreService: ScoreService, val scoreMapper: ScoreMapper) {
 
     @GetMapping
