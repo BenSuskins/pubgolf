c141f2d59f121a824136f28525a422ea6867486f
Ben Suskins <suskinsdevelopment@gmail.com>
2024-06-24 23:35:41 +0100

    - CORS

---
+++ src/main/kotlin/uk/co/suskins/pubgolf/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package uk.co.suskins.pubgolf.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    @Value("\${application.origins}")
+    private lateinit var origins: Array<String>
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(*origins)
+            .allowedMethods("GET", "POST", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)  // 1 hour max age
+    }
+}
