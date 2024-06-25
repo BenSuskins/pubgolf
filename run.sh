fec04ff389d4d29b0fc3df0c7c7e40d9a50a1f93
Ben Suskins <suskinsdevelopment@gmail.com>
2024-06-25 22:28:30 +0100

    Update dockerfile (#33)

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+docker stop pubgolf
+
 ./gradlew clean build
 
 docker build -t pubgolf .
 
-docker run -d -p 8081:8080 --name pubgolf pubgolf
+docker run -d -p 8080:8080 --name pubgolf pubgolf
