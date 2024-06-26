67eb3db977b5bde351f629d25b9f74514774dd13
Ben Suskins <suskinsdevelopment@gmail.com>
2024-06-26 21:34:46 +0100

    - Validate Name

--- src/main/kotlin/uk/co/suskins/pubgolf/api/JoinGameDto.kt
+++ src/main/kotlin/uk/co/suskins/pubgolf/api/JoinGameDto.kt
@@ -1,8 +1,11 @@
 package uk.co.suskins.pubgolf.api
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 
 data class JoinGameDto(
     @JsonProperty("name")
+    @field:Min(2) @field:Max(40)
     val name: String
 )
