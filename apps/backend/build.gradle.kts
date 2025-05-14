aa2d85254aab5b8695b7a974d6050d2aa1f41175
Ben Suskins <suskinsdevelopment@gmail.com>
2025-05-14 22:38:23 +0100

    Use okeydoke

--- apps/backend/build.gradle.kts
+++ apps/backend/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("com.natpryce:hamkrest:1.8.0.1")
-    testImplementation("com.approvaltests:approvaltests:18.0.0")
+    implementation("com.oneeyedmen:okeydoke:2.0.3")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- apps/backend/src/test/kotlin/uk/co/suskins/pubgolf/approvals/OpenApiTest.kt
+++ apps/backend/src/test/kotlin/uk/co/suskins/pubgolf/approvals/OpenApiTest.kt
@@ -1,19 +1,39 @@
 package uk.co.suskins.pubgolf.approvals
 
-import org.approvaltests.Approvals
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.oneeyedmen.okeydoke.Approver
+import com.oneeyedmen.okeydoke.junit5.ApprovalsExtension
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.web.client.RestTemplate
 
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class OpenApiTest {
+    @RegisterExtension
+    var approvals: ApprovalsExtension = ApprovalsExtension()
+
     @Test
-    fun `Can generate an open api spec`() {
-        Approvals.verify(openApiDocs())
+    fun `Can generate an open api spec`(approver: Approver) {
+        approver.assertApproved(openApiDocs().asPrettyJson())
     }
 
     private fun openApiDocs(): String? {
         val restTemplate = RestTemplate()
         return restTemplate.getForObject("http://localhost:8080/v3/api-docs", String::class.java)
+    }
+}
+
+private fun String?.asPrettyJson(): String {
+    if (this.isNullOrBlank()) return "<empty or null JSON>"
+
+    return try {
+        val mapper = ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT)
+        val jsonNode = mapper.readTree(this)
+        mapper.writeValueAsString(jsonNode)
+    } catch (e: Exception) {
+        "<invalid JSON>\n$this"
     }
 }
