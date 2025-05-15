e79516ca3946ff7d2fea2f3b2e4db38baa2d71b0
Ben Suskins <suskinsdevelopment@gmail.com>
2025-05-15 20:11:16 +0100

    Fix spelling

--- apps/backend/src/test/kotlin/uk/co/suskins/pubgolf/scenarios/ScenarioTest.kt
+++ apps/backend/src/test/kotlin/uk/co/suskins/pubgolf/scenarios/ScenarioTest.kt
@@ -1,5 +1,7 @@
 package uk.co.suskins.pubgolf.scenarios
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
@@ -11,4 +13,9 @@ abstract class ScenarioTest {
     fun createGame(): ResponseEntity<String> {
         return restTemplate.postForEntity("http://localhost:8080/api/v1/games", null, String::class.java)
     }
+}
+
+fun String?.asPrettyJson(): String {
+    val mapper = ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT)
+    return mapper.writeValueAsString(mapper.readTree(this))
 }
