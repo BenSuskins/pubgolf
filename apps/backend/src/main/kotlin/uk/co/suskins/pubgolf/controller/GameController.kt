7db22e350332e8e9dcda2e03339256b124d9840b
Ben Suskins <suskinsdevelopment@gmail.com>
2025-05-16 16:55:37 +0100

    Submit a score!

--- apps/backend/src/main/kotlin/uk/co/suskins/pubgolf/controller/GameController.kt
+++ apps/backend/src/main/kotlin/uk/co/suskins/pubgolf/controller/GameController.kt
@@ -30,12 +30,24 @@ class GameController {
             listOf(Player("player-xyz789", "Player", listOf(0, 0, 0, 0, 0, 0, 0, 0, 0)))
         )
     }
+
+    @PostMapping("/api/v1/games/{gameCode}/players/{playerId}/scores")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun submitScore(
+        @PathVariable("gameCode") gameCode: String,
+        @PathVariable("playerId") playerId: String,
+        @RequestBody scoreRequest: ScoreRequest
+    ) {
+        if (gameCode != "ABC123") throw GameNotFoundException("Game `$gameCode` could not be found.")
+    }
 }
 
 class GameNotFoundException(override val message: String?) : Exception()
 
 data class GameRequest(val host: String)
 data class GameJoinRequest(val name: String)
+data class ScoreRequest(val hole: Int, val score: Int)
+
 data class Game(val gameId: String, val gameCode: String, val playerId: String, val playerName: String)
 data class GameState(val gameId: String, val gameCode: String, val players: List<Player>)
 data class Player(val id: String, val name: String, val scores: List<Int>)
