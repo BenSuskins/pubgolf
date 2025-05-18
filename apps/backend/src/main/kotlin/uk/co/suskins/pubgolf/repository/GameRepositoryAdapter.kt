048701d53e5d2949f75fe7b3d94119c205598b7d
Ben Suskins <suskinsdevelopment@gmail.com>
2025-05-18 18:13:42 +0100

    Little bit of logging

--- apps/backend/src/main/kotlin/uk/co/suskins/pubgolf/repository/GameRepositoryAdapter.kt
+++ apps/backend/src/main/kotlin/uk/co/suskins/pubgolf/repository/GameRepositoryAdapter.kt
@@ -1,16 +1,20 @@
 package uk.co.suskins.pubgolf.repository
 
 import dev.forkhandles.result4k.*
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 import uk.co.suskins.pubgolf.models.*
 
 @Repository
 class GameRepositoryAdapter(private val store: GameJpaRepository) : GameRepository {
+    private val logger = LoggerFactory.getLogger(GameRepositoryAdapter::class.java)
     override fun save(game: Game): Result<Game, PubGolfFailure> {
         return resultFrom {
             val entity = game.toJpa()
             val saved = store.save(entity)
             saved.toDomain()
+        }.peekFailure {
+            logger.error("Error saving game `${game.code}.", it)
         }.mapFailure {
             PersistenceFailure(it.message ?: "Save failed")
         }
