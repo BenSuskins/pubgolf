6eaeecaf749249c0e0afd99646d8170df6e0566f
Ben Suskins <suskinsdevelopment@gmail.com>
2025-05-20 15:31:53 +0100

    Fix backend versioning

--- apps/backend/build.gradle.kts
+++ apps/backend/build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "2.1.21"
+    kotlin("plugin.spring") version "2.1.21"
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.jpa") version "2.1.21"
     id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }
 
 group = "uk.co.suskins"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 
 java {
     toolchain {
@@ -30,6 +30,8 @@ repositories {
 extra["springCloudVersion"] = "2024.0.1"
 extra["forkhandlesVersion"] = "2.22.3.0"
 extra["springDocVersion"] = "2.8.6"
+extra["hamkrestVersion"] = "1.8.0.1"
+extra["okeyDokeVersion"] = "2.0.3"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -51,9 +53,9 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("com.natpryce:hamkrest:1.8.0.1")
-    implementation("dev.forkhandles:result4k-hamkrest")
-    implementation("com.oneeyedmen:okeydoke:2.0.3")
+    testImplementation("com.natpryce:hamkrest:${property("hamkrestVersion")}")
+    testImplementation("dev.forkhandles:result4k-hamkrest")
+    testImplementation("com.oneeyedmen:okeydoke:${property("okeyDokeVersion")}")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
