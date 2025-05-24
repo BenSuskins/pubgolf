67cb962cc8622649c2177fccaf5ac6642b3cab24
Ben Suskins <suskinsdevelopment@gmail.com>
2025-05-24 10:40:29 +0100

    Clean up

--- apps/backend/src/main/kotlin/uk/co/suskins/pubgolf/models/Domain.kt
+++ apps/backend/src/main/kotlin/uk/co/suskins/pubgolf/models/Domain.kt
@@ -14,6 +14,7 @@ data class Player(
     val scores: Map<Int, Int> = initialScore()
 ) {
     fun matches(playerId: UUID) = id == playerId
+
     fun updateScore(
         hole: Int,
         score: Int
