8108380b4f5f8c3b5019940004b14d910936c89a
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-03 14:26:18 +0100

    Sentry

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -21,7 +21,7 @@ module.exports = withSentryConfig(
     // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
     org: "suskinsdevelopment",
-    project: "javascript-nextjs",
+    project: "pubgolf",
 
     // Only print logs for uploading source maps in CI
     silent: !process.env.CI,
