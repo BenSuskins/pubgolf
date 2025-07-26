dd3eb3bff8f73e331489330faf909539e25df246
Ben Suskins <suskinsdevelopment@gmail.com>
2025-07-26 14:33:34 +0100

    Update Entity.kt

--- apps/backend/src/main/kotlin/uk/co/suskins/pubgolf/models/Entity.kt
+++ apps/backend/src/main/kotlin/uk/co/suskins/pubgolf/models/Entity.kt
@@ -114,13 +114,15 @@ fun GameEntity.toDomain(): Game =
                             )
                         },
                     scores =
-                        it.scores.associate {
-                            Hole(it.id.hole) to
-                                ScoreWithTimestamp(
-                                    Score(it.score),
-                                    it.modified,
-                                )
-                        },
+										    it.scores
+										        .sortedBy { it.id.hole }
+										        .associateTo(LinkedHashMap()) {
+										            Hole(it.id.hole) to
+										                ScoreWithTimestamp(
+										                    Score(it.score),
+										                    it.modified,
+										                )
+										        },
                 )
             },
     )
